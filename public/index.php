2016bde5428f74c288f896ddd0ceba8fb5b003f3
Baiba Elste <124500527+baiba92@users.noreply.github.com>
2023-05-11 16:23:22 +0300

    update

--- public/index.php
+++ public/index.php
@@ -7,18 +7,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $routes = require_once '../routes.php';
 
-//$client = new \App\ApiClient();
-//$characters = $client->filterCharacters('rick', 'alive', 'human', 'male');
-
-//$controller = new \App\Controllers\CharacterController();
-//$characters = $controller->filter();
-//echo "<pre>";
-//var_dump($characters);
-//foreach ($characters as $character) {
-//    var_dump($character);
-//}
-//die;
-
 $response = Router::response($routes);
 $renderer = new Renderer();
 
